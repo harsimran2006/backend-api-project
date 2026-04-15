4b506687f6010227f2157e27d22dded7eed0cfe1
Harsimranpreet Kaur <Hkaur9496@conestogac.on.ca>
2026-04-14 21:14:35 -0400

    PROJ-7 Added authetication jwt verification

---
+++ src/presentation/middleware/authMiddleware.ts
@@ -0,0 +1,34 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+export interface AuthRequest extends Request {
+    user?: any;
+}
+
+export const authMiddleware = (
+    req: AuthRequest,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader) {
+            return res.status(401).json({ message: "No token provided" });
+        }
+
+        const token = authHeader.split(" ")[1];
+
+        if (!token) {
+            return res.status(401).json({ message: "Invalid token format" });
+        }
+
+        const decoded = jwt.verify(token, "secretkey");
+
+        req.user = decoded;
+
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "Invalid token" });
+    }
+};
--- src/presentation/route/authRoutes.ts
+++ src/presentation/route/authRoutes.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import { AuthController } from "../controllers/AuthController.js";
 
+import { authMiddleware } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
+
+router.get("/me", authMiddleware, (req: any, res) => {
+    res.json({
+        message: "Protected api authetication",
+        user: req.user
+    });
+});
 
 export const authRoutes = router;
